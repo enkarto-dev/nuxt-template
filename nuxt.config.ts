7e2899be40c9d9fe77623b3d122e4f6d6bc95a9c
Christine Nyries Yambao <christine.yambao@enkarto.com>
2023-10-09 16:21:37 +0800

    feat: add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: ["@pinia/nuxt"],
+  devtools: { enabled: true },
+  imports: {
+    dirs: ["stores"],
+  },
+  pinia: {
+    autoImports: ["defineStore", "acceptHMRUpdate"],
+  },
+});
