44699c6b2beb72d7e2e31646de2b27f0732c3b8e
Christine Nyries Yambao <christine.yambao@enkarto.com>
2023-10-09 18:06:55 +0800

    feat: add nuxt-image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@nuxt/image"],
   devtools: { enabled: true },
   imports: {
     dirs: ["stores"],
@@ -11,5 +11,17 @@ export default defineNuxtConfig({
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
     configPath: "tailwind.config",
+  },
+  image: {
+    providers: {
+      keycdn: {
+        name: "keycdn",
+        provider: "~/providers/keycdn",
+        options: {
+          baseURL: "https://cdn.enkarto.com/",
+        },
+      },
+    },
+    provider: "keycdn",
   },
 });
