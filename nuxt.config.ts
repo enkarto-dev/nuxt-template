b20318555f35ff86274680fa7808bb576e969a9a
Christine Nyries Yambao <christine.yambao@enkarto.com>
2023-10-09 16:49:18 +0800

    feat: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
   devtools: { enabled: true },
   imports: {
     dirs: ["stores"],
   },
   pinia: {
     autoImports: ["defineStore", "acceptHMRUpdate"],
+  },
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "tailwind.config",
   },
 });
